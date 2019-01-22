515b702dd4157c01673518a4cd2bba8b538aa77f
mliki melek <mmelek1001@gmail.com>
2019-01-22 04:36:09 +0100

    done Portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,122 @@
+let resumeData = {
+  imagebaseurl: "https://www.linkedin.com/in/mliki-melek-85a920148/",
+  name: "Melek Mliki",
+  role: "JavaScript Developer",
+  Gmail: "   mmelek1001@gmail.com",
+  //skypeid: "Your skypeid",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/mliki-melek-85a920148",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "http://github.com/MMelek00",
+      className: "fa fa-github"
+    },
+    {
+      name: "skype",
+      url: "",
+      className: "fa fa-twitter"
+    }
+  ],
+  aboutme:
+    "I have just finished my studies at the Institute of Computer Science and I am willing to do my master's degree next year. I am a self-taught Full Stack JS developer, and my challange every day is to learn more. I believe that to succeed in life, one must be obsessed with their dreams and continue to work for them.",
+  address: "Tunisia",
+  website: "(216) 27196094",
+  education: [
+    {
+      UniversityName:
+        "Higher institute of computer sciences and mathematics of Monastir ",
+      specialization:
+        "Applied Bachelor's Degree to Computer Science: Comuter Systems and Software",
+      MonthOfPassing: "2014 -",
+      YearOfPassing: "2018"
+    },
+    {
+      UniversityName: "Not decided Yet..",
+      specialization: "Ms in Computer Science",
+      MonthOfPassing: "Sep",
+      YearOfPassing: "2019"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "Freelance",
+      specialization: "Mobile Applications",
+      DateOfStarting: "Feb-2016",
+      DateOfLeaving: "Present",
+      Achievements:
+        "Developed several Android and IOS Application With React Native."
+    },
+    {
+      CompanyName: "Optimal solutions",
+      specialization: "Mobile Developer| Intern",
+      DateOfStarting: "Jan-2018",
+      DateOfLeaving: "Jun-2018",
+      Achievements: "Building mobile applications with React native"
+    }
+  ],
+  skillsDescription: "I'm not an expert but i'm trying to be",
+  skills: [
+    {
+      skillname: "ReactNative"
+    },
+    {
+      skillname: "JavaScript"
+    },
+    {
+      skillname: "Reactjs"
+    },
+    {
+      skillname: "NodeJs"
+    },
+    {
+      skillname: "MongoDb"
+    }
+  ],
+  portfolio: [
+    {
+      name: "Money-Man",
+      description: "my First mobile app ",
+      imgurl: "images/portfolio/firstapp.png"
+    },
+    {
+      name: "BusinessApp",
+      description: "Chat app as a freelancer",
+      imgurl: "images/portfolio/chatapp.jpg"
+    },
+    {
+      name: "mobile app",
+      description: "react native",
+      imgurl: "images/portfolio/app.jpg"
+    },
+    {
+      name: "...",
+      description: "...",
+      imgurl: "images/portfolio/the-best.jpg"
+    }
+  ],
+  testimonials: [
+    {
+      description:
+        "You don’t hire for skills, you hire for attitude. You can always teach skills",
+      name: "Simon Sinek"
+    },
+    {
+      description:
+        "Every great developer you know got ther by solving problems they were unqualified to solve until they actually did it",
+      name: "Patrick Mckenzie"
+    },
+    {
+      description:
+        "There are only two ways to influence human behavior: you can manipulate it or you can inspire it.",
+      name: "Simon Sinek"
+    }
+  ]
+};
+
+export default resumeData;
